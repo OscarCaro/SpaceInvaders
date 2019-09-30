af51dc2db300a8747f64929b9d3bf82de79b016c
Óscar Caro <oscaro@ucm.es>
2019-09-30 19:32:20 +0200

    Added CarrierList and DestroyerList constructors (need to be changed later)

--- src/tp/p1/CarrierList.java
+++ src/tp/p1/CarrierList.java
@@ -1,5 +1,15 @@
 package tp.p1;
 
 public class CarrierList {
-	//ni idea de que hace esto
+	
+	private Carrier[] carrierList;
+	
+	public CarrierList(int initNumOfCarriers) {
+		// The max amount of carriers at the same time is the initial number (determined by difficulty)
+		this.carrierList = new Carrier [initNumOfCarriers];
+		for (int i = 0; i < initNumOfCarriers; i++) {
+			// ToDo: for each new Carrier, determine position and uncomment the next
+			// carrierList[i] = new Carrier(bool isUfo, int posX, int posY);
+		}
+	}
 }
--- src/tp/p1/DestroyerList.java
+++ src/tp/p1/DestroyerList.java
@@ -1,5 +1,14 @@
 package tp.p1;
 
 public class DestroyerList {
-	//ni idea de que hace esto
-}
+	
+	private Destroyer[] destroyerList;
+	
+	public DestroyerList(int initNumOfDestroyers) {
+		// The max amount of carriers at the same time is the initial number (determined by difficulty)
+		this.destroyerList = new Destroyer [initNumOfDestroyers];
+		for (int i = 0; i < initNumOfDestroyers; i++) {
+			// ToDo: for each new Destroyer, determine position and uncomment the next
+			// destroyerList[i] = new Destroyer( int posX, int posY);
+		}
+	}}
