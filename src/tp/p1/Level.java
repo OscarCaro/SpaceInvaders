6347c47f3d2d4a7595b34d59cb23ccafbf3a8b07
Óscar Caro <oscaro@ucm.es>
2019-10-01 08:58:56 +0200

    Enum class for Difficulty argument (Easy, hard, insane)

--- src/tp/p1/Level.java
+++ src/tp/p1/Level.java
@@ -1,6 +1,43 @@
 package tp.p1;
 
-public class Level {
-	//Enumerado con los niveles
-	private enum dif_level {EASY, HARD, INSANE};
+public enum Level {
+	
+	EASY (4, 2, 0.1, 3, 0.5),
+	HARD (8, 2, 0.3, 2, 0.2),
+	INSANE (8, 4, 0.5, 1, 0.1);
+	
+	private int numOfCarriers;
+	private int numOfDestroyers;
+	private double fireFreq;
+	private int speed;			// CAUTION: measured in number of cycles per (single-square) movement -> The lower, the faster
+	private double ufoFreq;
+	
+	private Level(int numOfCarriers, int numOfDestroyers, double fireFreq, int speed, double ufoFreq) {
+		this.numOfCarriers = numOfCarriers;
+		this.numOfDestroyers = numOfDestroyers;
+		this.fireFreq = fireFreq;
+		this.speed = speed;
+		this.ufoFreq = ufoFreq;
+	}
+	
+	public int getNumOfCarriers () {
+		return numOfCarriers;
+	}
+	
+	public int getNumOfDestroyers() {
+		return numOfDestroyers;
+	}
+	
+	public double getFireFreq() {
+		return fireFreq;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public double getUfoFreq() {
+		return ufoFreq;
+	}
+	
 }
