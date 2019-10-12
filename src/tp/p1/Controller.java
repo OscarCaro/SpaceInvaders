1d473ceb9c7fc035af89fac073774e719f3ccd63
Tvring <55629023+Tvring@users.noreply.github.com>
2019-10-12 19:53:40 +0200

    Loop of the controller implemented

--- src/tp/p1/Controller.java
+++ src/tp/p1/Controller.java
@@ -38,93 +38,61 @@ public class Controller {
 	}
 
 	public void processInput(String input) {
-		// This function assumes that the String input is already taken from the CMD
-		
-		// THIS IS WRONG, can't create a new ucmShip here
-		// Instead, create a function in class UcmShip to move it to right/left or whatever
-		// and call that one from here, given that the UcmShip should already be created from outside of here
-		int positionX = 4;
-		int positionY = 7;
-		
-		//UCM_Ship player = new UCM_Ship(positionX,positionY);
 		
 		input = input.toLowerCase();	
 		
-		if (input.equals("move left 1") || input.equals("m left 1")) {	
-			this.game.moveUcmShip(true, false, 1);
-//			if(positionX == 0) {
-//				positionX = 8;
-//			}			
-//			else {
-//				positionX = positionX -1;
-//			}			
-		} 
-		else if (input.equals("move left 2") || input.equals("m left 2")) {	
-			this.game.moveUcmShip(true, false, 2);
-
-//			if(positionX == 0) {
-//				positionX = 7;
-//			}		
-//			else if(positionX == 1) {
-//				positionX = 8;
-//			}
-//			else {
-//				positionX = positionX -2;
-//			}			
-		}
-		else if (input.equals("move right 1") || input.equals("m right 1")) {
-			this.game.moveUcmShip(false, true, 1);
-
-//			if(positionX == 8) {
-//				positionX = 0;
-//			}			
-//			else {
-//				positionX = positionX +1;
-//			}
-		}
-		else if (input.equals("move right 2") || input.equals("m right 2")) {
-			this.game.moveUcmShip(false, true, 2);
-//			if(positionX == 8) {
-//				positionX = 1;
-//			}
-//			else if (positionX == 7) {
-//				positionX = 0;
-//			}
-//			else {
-//				positionX = positionX +2;
-//			}
-		}
+		while(!input.equals("exit") || !input.equals("e")) {
+			System.out.println("Command > ");
+			input = in.nextLine();
+			input = input.toLowerCase();
+			
+			if (input.equals("move left 1") || input.equals("m left 1")) {	
+				this.game.moveUcmShip(true, false, 1);
 		
-		else if (input.equals("shoot") || input.equals("s")) {
+			} 
+			else if (input.equals("move left 2") || input.equals("m left 2")) {	
+				this.game.moveUcmShip(true, false, 2);
 			
-		}
-//		else if (input == "shockwave" || input == "w") {
-//			if(player.shock == true) {
-//				
-//			}			
-//			else {
-//				System.out.println("Shockwave not available!!");
-//			}
-//		}
-		else if (input.equals("none") || input.equals("n") || input.equals("")) {
+			}
+			else if (input.equals("move right 1") || input.equals("m right 1")) {
+				this.game.moveUcmShip(false, true, 1);
+
+			}
+			else if (input.equals("move right 2") || input.equals("m right 2")) {
+				this.game.moveUcmShip(false, true, 2);
+
+			}
+		
+			else if (input.equals("shoot") || input.equals("s")) {
 			
-		}
-		else if (input.equals("list") || input.equals("l")) {
+			}
+
+			else if (input.equals("none") || input.equals("n") || input.equals("")) {
 			
-		}
-		else if (input.equals("reset") || input.equals( "r")) {
+			}
 			
-		}
-		else if (input.equals( "help") ||input.equals("h")) {
+			else if (input.equals("list") || input.equals("l")) {
 			
-		}
-		else if (input.equals("exit") || input.equals("e")) {
+			}
 			
-		}
-		else {
+			else if (input.equals("reset") || input.equals( "r")) {
+			
+			}
+			
+			else if (input.equals( "help") ||input.equals("h")) {
+			
+			}
+			
+			else if (input.equals("exit") || input.equals("e")) {
+			
+			}
+			
+			else {
 			// Error Message to ask again for input
+			}
+			
+			System.out.println(game); 
 		}
-		
 	}		
 	
 }
