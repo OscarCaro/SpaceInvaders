2f8c25b5267498edec8b374fa49cd5d7d06bd2e3
Óscar Caro <oscaro@ucm.es>
2019-10-14 09:40:36 +0200

    Method to compute number of valid Projectiles in projectileList of BombList

--- src/tp/p1/BombList.java
+++ src/tp/p1/BombList.java
@@ -6,7 +6,25 @@ public class BombList {
 	private Bomb [] projectileList;
 
 	public BombList(Level diffLevel) {
-		this.projectileList = new Bomb[];
+		// at most one projectile from each destroyer at a given time
+		// then the max size of list is the starting number of destroyers
+		this.projectileList = new Bomb[diffLevel.getNumOfDestroyers()];	
+		// ucmMissile will be created/set-to-null at run time
+	}
+	
+	public boolean isThereUcmMissile () {
+		return (ucmMissile != null);		
+	}
+	
+	public int getNumOfValidProjectiles() {
+		// Return num of instantiated bombs in projectileList (i.e. num of not null elems)
+		int count = 0;
+		for (Bomb current : this.projectileList) {
+			if (current != null) {
+				count++;
+			}
+		}
+		return count;
 	}
 	
 }
