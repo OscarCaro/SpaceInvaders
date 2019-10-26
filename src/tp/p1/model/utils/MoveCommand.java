dfbf8d7453a3f2e25ab3fb82dee920928769be22
Tvring <55629023+Tvring@users.noreply.github.com>
2019-10-27 01:17:11 +0200

    Command SubClasses created

---
+++ src/tp/p1/model/utils/MoveCommand.java
@@ -0,0 +1,25 @@
+package tp.p1.model.utils;
+
+import tp.p1.model.Command;
+import tp.p1.model.Game;
+
+public class MoveCommand extends Command {
+
+	public MoveCommand(String name, String shortName, String commandText, String helpText) {
+		super(name, shortName, commandText, helpText);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean execute(Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Command parse(String[] commandWords) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
