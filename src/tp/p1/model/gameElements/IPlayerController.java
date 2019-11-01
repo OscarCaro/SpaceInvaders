21df1f80e66a5f17d78b5de42f17d9f39ee16a07
Óscar Caro <oscaro@ucm.es>
2019-11-01 21:08:46 +0100

    Alberto's previous commits added

---
+++ src/tp/p1/model/gameElements/IPlayerController.java
@@ -0,0 +1,12 @@
+package tp.p1.model.gameElements;
+
+public interface IPlayerController {
+	// Player actions
+	public boolean move (int numCells);
+	public boolean shootMissile();
+	public boolean shockWave();
+	// Callbacks 
+	public void receivePoints(int points);
+	public void enableShockWave();
+	public void enableMissile();
+}
