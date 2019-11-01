b221684487067e8f3671bfd7ab370b5ddefc7056
Óscar Caro <oscaro@ucm.es>
2019-11-01 21:00:35 +0100

    Revert changes to afternoon's version

--- src/tp/p1/model/gameElements/IAttack.java
+++
@@ -1,8 +0,0 @@
-package tp.p1.model.gameElements;
-
-public interface IAttack {
-	default boolean performAttack(GameObject other) {return false;};
-	default boolean receiveMissileAttack(int damage) {return false;};
-	default boolean receiveBombAttack(int damage) {return false;};
-	default boolean receiveShockWaveAttack(int damage) {return false;};
-}
