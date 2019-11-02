3c28ec900691f1360e3f8432fdeeb4db040058b3
Óscar Caro <oscaro@ucm.es>
2019-11-02 16:25:07 +0100

    Little fix in RandomAction interface

--- src/tp/p1/model/gameElements/IExecuteRandomActions.java
+++ src/tp/p1/model/gameElements/IExecuteRandomActions.java
@@ -3,10 +3,12 @@ package tp.p1.model.gameElements;
 import tp.p1.model.Game;
 
 public interface IExecuteRandomActions {
+	
 	static boolean canGenerateUfo(Game game){
-		return game.getRandom().nextInt(10) < 10*game.getLevel().getOvniFrequency();
-		}
+		return game.getRandom().nextInt(10) < 10*game.getDiffLevel().getUfoFrequency();
+	}
+	
 	static boolean canGenerateBomb(Game game){
-		return game.getRandom().nextInt(10) < 10*game.getLevel().getShootFrequency();
-		}
+		return game.getRandom().nextInt(10) < 10*game.getDiffLevel().getShootFrequency();
+	}
 }
